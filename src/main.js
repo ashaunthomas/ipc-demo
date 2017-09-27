e72f09c0884160129ead8c32bca33058683034b5
Ashaun Thomas <ashaunthomas@siu.edu>
2017-09-27 13:02:57 -0500

    window initialized

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
 
 const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 /* basic ready function call syntax*/
 app.on('ready', _ => {
-    console.log("ready");
+    new BrowserWindow({
+        height: 400,
+        width: 400
+    })
 });
