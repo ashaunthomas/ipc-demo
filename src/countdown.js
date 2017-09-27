2ced8dc35b33fde9d327bf0b4f522df035063f46
ashaunthomas <ashaunthomas@siu.edu>
2017-09-27 15:32:19 -0500

    simple style stuff

--- src/countdown.js
+++ src/countdown.js
@@ -2,10 +2,11 @@ module.exports = function countdown(tick) {
     let count = 10;
 
     let timer = setInterval(_ => {
-        tick(count--);
         console.log("count" , count);
         if  (count === 0) {
             clearInterval(timer);
+            console.log("done");
         }
+        tick(count--);
     }, 1000);
 }
