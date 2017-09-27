9884e0f6bdbafe7ae9b147d67da315e6e83ea660
Ashaun Thomas <ashaunthomas@siu.edu>
2017-09-27 13:06:18 -0500

    proper window closing protocol implemented

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+let mainWindow;
+
 /* basic ready function call syntax*/
 app.on('ready', _ => {
-    new BrowserWindow({
+    mainWindow = new BrowserWindow({
         height: 400,
         width: 400
     })
+    
+    mainWindow.on('closed', _ => {
+        console.log("closed");
+        mainWindow = null;
+    });
 });
+
